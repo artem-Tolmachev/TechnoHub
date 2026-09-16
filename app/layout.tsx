780ed0c38030f580d5fed996d259277d0cbeb9d4
artem <89ufisum@gmail.com>
2026-09-16 15:15:29 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="ru"
       className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${ibmPlexSans.variable} h-full antialiased`}
     >
       <body className="min-h-full">
